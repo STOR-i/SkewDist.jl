4c5cad153b19def257c7738ebfc05cadbefd1169
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-04-14 17:28:31 +0100

    Added location and scale parameters to MvSkewNormal type

--- src/mvskewnormal.jl
+++ src/mvskewnormal.jl
@@ -1,22 +1,39 @@
+# See "Statistical Applications of the multivariate skew-normal distribution" by Azzalini and Capitanio (1998)
+# for notations and explanations
+
+using Distributions: φ, Φ
+
+function scale_and_cor(Ω::Matrix{Float64})
+    Ωz = similar(Ω)
+    d = size(Ω,1)
+    ω = sqrt(diag(Ω))
+    for i in 1:d, j in 1:d
+        Ωz[i,j] = Ω[i,j]/(ω[i]*ω[j])
+    end
+    diagm(ω), Ωz
+end
+
 immutable MvSkewNormal <: Sampleable{Multivariate, Continuous}
     ξ::Vector{Float64}      # Location vector
-    Ω::PDMat                # Covariance matrix
+    ω::Matrix{Float64}      # Scale (diagonal matrix)
+    Ωz::PDMat               # Correlation matrix
     α::Vector{Float64}      # Shape vector
     # Auxiliary parameters
     δ::Vector{Float64}
     Ωstar::PDMat  # For sampling...
-    function MvSkewNormal(Ω::Matrix{Float64}, α::Vector{Float64})
+    function MvSkewNormal(ξ::Vector{Float64}, Ω::Matrix{Float64}, α::Vector{Float64})
+        ω, Ωz = scale_and_cor(Ω)
         d = length(α)
-        Ωα = Ω*α
-        αΩα = dot(α, Ωα)
-        δ = Ωα/sqrt(1+αΩα)
+        Ωzα = Ωz*α
+        αΩzα = dot(α, Ωzα)
+        δ = Ωzα/sqrt(1+αΩzα)
         Ωstar = Array(Float64, d+1, d+1)
         Ωstar[:, 1] = [1, δ]
         Ωstar[1,:] = [1 δ']
-        Ωstar[2:end, 2:end] = Ω
+        Ωstar[2:end, 2:end] = Ωz
         Ωstar = PDMat(Ωstar)
-        Ω = PDMat(Ω)
-        new(Ω, α, δ, Ωstar)
+        Ωz = PDMat(Ωz)
+        new(ξ, ω, Ωz, α, δ, Ωstar)
     end
 end
 
@@ -27,15 +44,66 @@ function _rand!{T<:Real}(dist::MvSkewNormal, out::AbstractVector{T})
     unwhiten!(dist.Ωstar, xx)
     x0 = xx[1]
     x = xx[2:end]
-    for i in 1:length(dist)
-        out[i] = (x0 > 0 ? x[i] : -x[i])
-    end
+    copy!(out, dist.ω * (x0 > 0 ? x : -x) + dist.ξ)
 end
 
-mean(dist::MvSkewNormal) = sqrt(2/π)*dist.δ
-var(dist::MvSkewNormal) = diag(dist.Ω.mat) - mean(dist).^2
+mean(dist::MvSkewNormal) = sqrt(2/π)*dist.ω*dist.δ + dist.ξ
+var(dist::MvSkewNormal) = (dist.ω.^2)*(diag(dist.Ωz.mat) - (sqrt(2/π)*dist.δ).^2)
 
 function cov(dist::MvSkewNormal)
-    μ=mean(dist)
-    dist.Ω.mat - μ*μ'
+    μz=sqrt(2/π)*dist.δ
+    dist.ω * (dist.Ωz.mat - μz*μz') * dist.ω
 end
+
+# Special functions (related to cumulant function of half-normal distribution)
+ζ₀(x::Float64) = log(2π * Φ(x))
+ζ₀(x::Vector{Float64}) = map(ζ₀, x)
+
+ζ₁(x::Float64) = φ(x)/Φ(x)
+ζ₁(x::Vector{Float64}) = map(ζ₁, x)
+
+# Fit a MvSkewNormal regression model for model to data
+# X = (n x p) model matrix (each row is corresponds to the predictors of one response)
+# Y = (n x k) response matrix (each row correspond to one output observation)
+#
+# The following model is assumed:
+#   yᵢ ∼ SNₖ(ξᵢ, Ω, α) where ξᵢ = xᵢ̱β
+# for some (p x k) matrix β of parameters
+function fit_skew(X::Matrix{Float64}, Y::Matrix{Float64})
+    size(X,1) == size(Y,1) || throw(ArgumentError("X and Y must have the same number of rows"))
+    n,p = size(X)
+    k = size(Y,2)
+    # β is a (p x k) parameter matrix
+    u(β::Matrix{Float64}) = Y - X*β
+    V(β::Matrix{Float64}) = (u(β)'u(β))/n
+
+    function ll(params::Vector{Float64})
+        β = reshape(params[1:(p*k)], p, k)
+        η = params[p*k+1:end]
+        -0.5 * n * log(det(V(β))) - 0.5 * n * k + sum(ζ₀(u(β)*η))
+    end
+    
+    function dll!(params::Vector{Float64}, grad::Vector{Float64})
+        β = reshape(params[1:(p*k)], p, k)
+        η = params[p*k+1:end]
+        
+         ∂l∂β = X'u(β)*inv(V(β)) - X'ζ₁(u(β)*η)*η'
+        
+        ∂l∂η= u(β)'ζ₁(u*η)
+        grad[1:p*k] = vec(∂l∂β)
+        grad[p*k + 1:end] = ∂l∂η
+    end
+
+    function ll_and_dll!(params::Vector{Float64}, grad::Vector{Float64})
+        β = reshape(params[1:p*k], p, k)
+        η = params[p*k+1:end]
+
+         ∂l∂β = X'u(β)*inv(V(β)) - X'ζ₁(u(β)*η)*η'
+        ∂l∂η = u(β)'ζ₁(u*η)
+        grad[1:p*k] = vec(∂l∂β)
+        grad[p*k + 1:end] = ∂l∂η
+        
+        -0.5 * n * log(det(V(β))) - 0.5 * n * k + sum(ζ₀(u(β)*η))
+    end
+    
+end    
--- test/test_MvSkewNormal.jl
+++ test/test_MvSkewNormal.jl
@@ -1,3 +1,5 @@
+using SkewDist, Distributions, Base.Test
+
 function cov2cor(X::Matrix{Float64})
     A = similar(X)
     d = size(X,1)
@@ -8,6 +10,12 @@ function cov2cor(X::Matrix{Float64})
     A
 end
 
+function rand_pd(d::Int)
+    A = randn(d,d)
+    A'A
+end
+
+
 function rand_cor(d::Int)
     A = randn(d,d)
     Σ = A'A
@@ -17,10 +25,11 @@ end
 srand(1)
 N = 100000 # Sample size
 
+ξ = [0.0, -0.1]
 α = [1.0, 0.5]
-Ω = rand_cor(2)
+Ω = rand_pd(2)
 
-dist = MvSkewNormal(Ω, α)
+dist = MvSkewNormal(ξ, Ω, α)
 rand(dist)
 
 Z = rand(dist, N)
@@ -28,4 +37,3 @@ Z = rand(dist, N)
 @test_approx_eq_eps mean(Z, 2) mean(dist) 1e-2
 @test_approx_eq_eps var(Z, 2) var(dist) 1e-2
 @test_approx_eq_eps cov(Z, vardim=2) cov(dist) 1e-2
-
