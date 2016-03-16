c84d8f9e7c6d4eb30a146d69159c1afc7b45586e
Jamie Fairbrother <jfairbrother87@hotmail.com>
2016-03-16 16:44:35 +0000

    Fixed SkewDist.jl

--- src/SkewDist.jl
+++ src/SkewDist.jl
@@ -2,14 +2,15 @@ module SkewDist
 
 using Distributions, StatsFuns, MultivariateStats, PDMats, Optim, Roots
 import Base: length, mean, show, rand, var, cov, *
-import Distributions: _rand!, pdf, _logpdf, _logpdf!, dof, cdf, quantile
+import Distributions: _rand!, pdf, _logpdf, _logpdf!, dof, cdf, quantile, minimum, maximum
 
-export MvSkewNormal, MvSkewTDist, SkewNormal, SkewTDist, fit_MvSkewNormal, fit_MvSkewTDist, marginals
+export MvSkewNormal, MvSkewTDist, SkewNormal, SkewTDist, fit_MvSkewNormal, fit_MvSkewTDist, marginals, mgf
 export pdf, dof, cdf, quantile
 
 include("utils.jl")
 include("mvskewnormal.jl")
 include("mvskewtdist.jl")
+include("skewnormal.jl")
 include("skewtdist.jl")
 include("operators.jl")
 include("fit_mvskewtdist.jl")
